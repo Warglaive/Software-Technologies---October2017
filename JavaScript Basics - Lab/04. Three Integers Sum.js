81247b2d20063f1c309317f7b642d8a3fbd2b29a
warglaive <warglaivegm@gmail.com>
2017-11-13 13:21:00 +0200

    refact

--- JavaScript Basics - Lab/04. Three Integers Sum.js
+++ JavaScript Basics - Lab/04. Three Integers Sum.js
@@ -1,16 +1,13 @@
 function threeIntegersSum(arr) {
     let nums = arr[0].split(' ').map(Number);
 
-    if(nums[0] + nums[1] == nums[2]){
+    if (nums[0] + nums[1] == nums[2]) {
         console.log(`${Math.min(nums[0],nums[1])} + ${Math.max(nums[0],nums[1])} = ${nums[2]}`)
-    }
-    else if(nums[0] + nums[2] == nums[1]){
+    } else if (nums[0] + nums[2] == nums[1]) {
         console.log(`${Math.min(nums[0],nums[2])} + ${Math.max(nums[0],nums[2])} = ${nums[1]}`)
-    }
-    else if(nums[1] + nums[2] == nums[0]){
+    } else if (nums[1] + nums[2] == nums[0]) {
         console.log(`${Math.min(nums[1],nums[2])} + ${Math.max(nums[1],nums[2])} = ${nums[0]}`)
-    }
-    else{
+    } else {
         console.log('No')
     }
 }
