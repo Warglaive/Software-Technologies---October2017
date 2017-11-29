c32d5f4862ee0f1ea06a62aac457ecb375d4e3fc
warglaive <warglaivegm@gmail.com>
2017-11-29 16:39:32 +0200

    7/7

--- Java Basics - Labx/src/SumsbyTown.java
+++ Java Basics - Labx/src/SumsbyTown.java
@@ -4,10 +4,9 @@ import java.util.TreeMap;
 public class SumsbyTown {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n = Integer.parseInt(scanner.nextLine());
         TreeMap<String, Double> sumsByTown = new TreeMap<>();
         for (int i = 0; i < n; i++) {
-            String str = scanner.nextLine();
             String[] tokens = scanner.nextLine().split("\\|");
             String town = tokens[0].trim();
             double income = Double.parseDouble(tokens[1].trim());
