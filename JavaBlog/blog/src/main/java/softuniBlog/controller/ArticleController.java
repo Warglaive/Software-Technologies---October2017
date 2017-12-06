c1c669039f00abd7cf255ba76240427653e61e64
warglaivee <warglaivegm@gmail.com>
2017-12-07 01:05:09 +0200

    up

--- JavaBlog/blog/src/main/java/softuniBlog/controller/ArticleController.java
+++ JavaBlog/blog/src/main/java/softuniBlog/controller/ArticleController.java
@@ -2,9 +2,13 @@ package softuniBlog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuniBlog.bindingModel.ArticleBindingModel;
 import softuniBlog.repository.ArticleRepository;
 import softuniBlog.repository.UserRepository;
 
@@ -14,11 +18,20 @@ public class ArticleController {
     private ArticleRepository articleRepository;
     @Autowired
     private UserRepository userRepository;
-    
+
     @GetMapping("/article/create")
     @PreAuthorize("isAuthenticated()")
     public String create(Model model) {
         model.addAttribute("view", "article/create");
         return "base-layout";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/article/create")
+    public String createProcess(ArticleBindingModel articleBindingModel) {
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
     }
 }
