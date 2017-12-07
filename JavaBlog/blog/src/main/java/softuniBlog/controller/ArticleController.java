0f9a94dc5951ad62d577f8f45bf9cb8afd3ccd9a
warglaivee <warglaivegm@gmail.com>
2017-12-07 03:41:16 +0200

    done 100%

--- JavaBlog/blog/src/main/java/softuniBlog/controller/ArticleController.java
+++ JavaBlog/blog/src/main/java/softuniBlog/controller/ArticleController.java
@@ -7,12 +7,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import softuniBlog.bindingModel.ArticleBindingModel;
 import softuniBlog.entity.Article;
 import softuniBlog.entity.User;
 import softuniBlog.repository.ArticleRepository;
 import softuniBlog.repository.UserRepository;
+
+import javax.jws.WebParam;
 
 @Controller
 public class ArticleController {
@@ -42,6 +45,68 @@ public class ArticleController {
                 userEntity
         );
         this.articleRepository.saveAndFlush(articleEntity);
+        return "redirect:/";
+    }
+
+    @GetMapping("/article/{id}")
+    public String details(Model model, @PathVariable Integer id) {
+        if (!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        model.addAttribute("article", article);
+        model.addAttribute("view", "article/details");
+        return "base-layout";
+    }
+
+    @GetMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String edit(@PathVariable Integer id, Model model) {
+        if (!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        model.addAttribute("view", "article/edit");
+        model.addAttribute("article", article);
+        return "base-layout";
+    }
+
+    @PostMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String editProcess(@PathVariable Integer id, ArticleBindingModel articleBindingModel) {
+        if (!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        article.setContent(articleBindingModel.getContent());
+        article.setContent(articleBindingModel.getContent());
+        this.articleRepository.saveAndFlush(article);
+        return "redirect:/article/" + article.getId();
+    }
+
+
+    //delete view page - WORKING
+    @GetMapping("article/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String delete(Model model, @PathVariable Integer id) {
+        if (!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        model.addAttribute("view", "article/delete");
+        model.addAttribute("article", article);
+        return "base-layout";
+    }
+
+    //BUG HERE
+    @PostMapping("article/delete/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String deleteProcess(Model model, @PathVariable Integer id) {
+        if (!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article article = this.articleRepository.findOne(id);
+        this.articleRepository.delete(article);
         return "redirect:/";
     }
 }
