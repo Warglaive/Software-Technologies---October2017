6d69521afaf5993184f7d400a7808bd6f8dfaa3e
warglaive <warglaivegm@gmail.com>
2017-12-07 15:58:25 +0200

    calc done

---
+++ 01. Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+        public double LeftOperand { get; set; }
+        public double RightOperand { get; set; }
+        public string Operator { get; set; }
+        public double Result { get; set; }
+    }
+}
