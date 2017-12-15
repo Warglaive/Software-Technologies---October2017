06ac2681d3dacbba5b99cd4a1d8fd2c5cffae2e0
warglaive <warglaivegm@gmail.com>
2017-12-15 21:18:21 +0200

    JS Done

--- TODO ListExamPrep2/JavaScript Skeleton/controllers/task.js
+++ TODO ListExamPrep2/JavaScript Skeleton/controllers/task.js
@@ -2,19 +2,39 @@ const Task = require('../models/Task');
 
 module.exports = {
     index: (req, res) => {
-        //TODO: Implement me...
-		res.render('task/index', {'tasks': null});
+        Task.find().then(tasks => {
+            res.render('task/index', {'tasks': tasks})
+        });
+
     },
-	createGet: (req, res) => {
-        //TODO: Implement me...
-	},
-	createPost: (req, res) => {
-        //TODO: Implement me...
-	},
-	deleteGet: (req, res) => {
-        //TODO: Implement me...
-	},
-	deletePost: (req, res) => {
-        //TODO: Implement me...
-	}
+    createGet: (req, res) => {
+        res.render('task/create');
+    },
+    createPost: (req, res) => {
+        let task = req.body;
+        Task.create(task).then(task => {
+            res.redirect("/");
+        }).catch(err => {
+            task.error = 'Cannot create task.';
+            res.render('task/create', task);
+        });
+    },
+    deleteGet: (req, res) => {
+        let taskId = req.params.id;
+        Task.findById(taskId).then(task => {
+            if (task) {
+                return res.render('task/delete', task);
+            }
+            else {
+                return res.redirect('/');
+            }
+        }).catch(err => res.redirect('/'));
+    },
+
+    deletePost: (req, res) => {
+        let taskId = req.params.id;
+        Task.findByIdAndRemove(taskId).then(task => {
+            res.redirect('/');
+        }).catch(err => res.redirect('/'));
+    },
 };
