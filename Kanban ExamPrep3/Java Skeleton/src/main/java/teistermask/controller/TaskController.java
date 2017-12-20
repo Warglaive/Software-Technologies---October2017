596ada4143ac4632775051e65de4005339e977b4
warglaivee <warglaivegm@gmail.com>
2017-12-20 20:04:01 +0200

    Java done

--- Kanban ExamPrep3/Java Skeleton/src/main/java/teistermask/controller/TaskController.java
+++ Kanban ExamPrep3/Java Skeleton/src/main/java/teistermask/controller/TaskController.java
@@ -7,39 +7,63 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import teistermask.bindingModel.TaskBindingModel;
+import teistermask.entity.Task;
 import teistermask.repository.TaskRepository;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
-	private final TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
-	@Autowired
-	public TaskController(TaskRepository taskRepository) {
-		this.taskRepository = taskRepository;
-	}
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
-	@GetMapping("/")
-	public String index(Model model) {
-		// TODO: Implement me...
-	}
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Task> openTasks = this.taskRepository.findAllByStatus("Open");
+        List<Task> inProgressTasks = this.taskRepository.findAllByStatus("In Progress");
+        List<Task> finishedTasks = this.taskRepository.findAllByStatus("Finished");
 
-	@GetMapping("/create")
-	public String create(Model model) {
-		// TODO: Implement me...
-	}
+        model.addAttribute("openTasks", openTasks);
+        model.addAttribute("inProgressTasks", inProgressTasks);
+        model.addAttribute("finishedTasks", finishedTasks);
 
-	@PostMapping("/create")
-	public String createProcess(Model model, TaskBindingModel taskBindingModel) {
-		// TODO: Implement me...
-	}
+        model.addAttribute("view", "task/index");
+        return "base-layout";
+    }
 
-	@GetMapping("/edit/{id}")
-	public String edit(Model model, @PathVariable int id) {
-		// TODO: Implement me...
-	}
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("view", "task/create");
+        return "base-layout";
+    }
 
-	@PostMapping("/edit/{id}")
-	public String editProcess(@PathVariable int id, TaskBindingModel taskBindingModel) {
-		// TODO: Implement me...
-	}
+    @PostMapping("/create")
+    public String createProcess(Model model, TaskBindingModel taskBindingModel) {
+        Task task = new Task();
+        task.setTitle(taskBindingModel.getTitle());
+        task.setStatus(taskBindingModel.getStatus());
+        taskRepository.saveAndFlush(task);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable int id) {
+        Task task = this.taskRepository.findOne(id);
+        model.addAttribute("view", "task/edit");
+        model.addAttribute("task", task);
+        return "base-layout";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProcess(@PathVariable int id, TaskBindingModel taskBindingModel) {
+        Task taskFromDB = this.taskRepository.findOne(id);
+        taskFromDB.setTitle(taskBindingModel.getTitle());
+        taskFromDB.setStatus(taskBindingModel.getStatus());
+        this.taskRepository.saveAndFlush(taskFromDB);
+        return "redirect:/";
+    }
 }
--- Kanban ExamPrep3/Java Skeleton/src/main/java/teistermask/entity/Task.java
+++ Kanban ExamPrep3/Java Skeleton/src/main/java/teistermask/entity/Task.java
@@ -5,5 +5,41 @@ import javax.persistence.*;
 @Entity
 @Table(name = "tasks")
 public class Task {
-	// TODO: Implement me...
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Task(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
