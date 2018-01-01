66ab45d1158bdc0b556c6ae1749d20f8d05fbf22
warglaive <warglaivegm@gmail.com>
2018-01-01 23:50:42 +0200

    exam up

---
+++ Exam21.12.2017/CSharp-Skeleton/LogNoziroh/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogNoziroh.Models
+{
+    public class Report
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public string Origin { get; set; }
+    }
+}
